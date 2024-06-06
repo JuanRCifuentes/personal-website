ddc54f892ee3436d2b81a3f87cf266940a5530f8
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2024-06-06 09:46:22 -0500

    Refactor layout component to improve code organization and readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,17 @@
+import type { Metadata } from 'next'
+import { Roboto_Flex } from 'next/font/google'
+import './globals.css';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 
-import './globals.css';
-
-import type { Metadata } from 'next'
-import { Roboto_Flex } from 'next/font/google'
-
-const roboto = Roboto_Flex({
-  subsets: ['latin'],
-})
+const roboto = Roboto_Flex({ subsets: ['latin'] })
  
 export const metadata: Metadata = {
   title: 'Juan R. Cifuentes',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout( {children}: {children: React.ReactNode} ) {
   return (
     <html lang="en" className={roboto.className}>
       <body className='dark:bg-navy dark:text-soft-gray'>
