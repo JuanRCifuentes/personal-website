beedea51b9911ccf4f331414628f858e86b8d25f
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2024-06-06 21:12:44 -0500

    refactor: Update contact page with email contactform

--- src/app/contact/contact.tsx
+++ src/app/contact/contact.tsx
@@ -1,12 +1,39 @@
 "use client"
 
 import { useAppContext } from '@/context/appContext';
+import { useState } from 'react';
 
 const Contact = () => {
   const { contactLinks } = useAppContext()
 
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    console.log(event)
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    formData.append("access_key", "0a1ff28f-163c-4ea5-ae43-fe3c39ed21cf");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      (event.target as HTMLFormElement).reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
-    <section className="py-14">
+    <section className="py-10">
         <div className="max-w-screen-xl mx-auto px-4 text-gray-600 gap-12 sm:px-8 lg:flex">
             <div className="max-w-md">
                 <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
@@ -32,7 +59,7 @@ const Contact = () => {
                                 <p>
                                     {item.message}
                                 </p>
-                                <a href={item.url} target='_blank' className="flex items-center gap-1 text-sm text-indigo-600 duration-150 hover:text-indigo-400 font-medium">
+                                <a href={item.url} target='_blank' className="flex items-center gap-1 text-sm text-blue-900 duration-150 hover:text-blue-500 font-medium">
                                     {item.urlName}
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"> <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" /> </svg>
                                 </a>
@@ -53,7 +80,7 @@ const Contact = () => {
                                 <p>
                                     {item.message}
                                 </p>
-                                <a href={item.url} target='_blank' className="flex items-center gap-1 text-sm text-indigo-600 duration-150 hover:text-indigo-400 font-medium">
+                                <a href={item.url} target='_blank' className="flex items-center gap-1 text-sm text-blue-900 duration-150 hover:text-blue-500 font-medium">
                                     {item.urlName}
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"> <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" /> </svg>
                                 </a>
@@ -63,6 +90,78 @@ const Contact = () => {
                 </ul>
             </div>
         </div>
+        <span className='hidden w-3/4 h-px bg-gray-200 md:block mx-auto mt-10 mb-5'></span>
+        <main>
+            <div className="sm:mt-14 md:mt-3 w-full mx-auto px-4 text-gray-600 md:px-8 sm:px-8">
+                <div className="mx-auto gap-16 lg:flex lg:max-w-none">
+                    <div className="max-w-lg basis-1/3 space-y-3">
+                        <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                            Or just say hi!
+                        </p>
+                        <p>
+                            You can also send me a message directly using the form below. I&apos;ll get back to you as soon as possible.
+                        </p>
+                        <p>
+                            Every field is optional, but if you want me to get back to you, please provide an email address.
+                        </p>
+                    </div>
+                    <div className='flex justify-center'>
+                        <div className="max-w-lg flex-1 mt-12">
+                            <form
+                                onSubmit={onSubmit}
+                                className="space-y-5"
+                            >
+                                <div>
+                                    <label className="font-medium">
+                                        Full name <a className='text-gray-400'>...or a nickname :D</a>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        required
+                                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="font-medium">
+                                        Email <a className='text-gray-400'>...so i can get back to you</a>
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name='email'
+                                        required
+                                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-blue-900 shadow-sm rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="font-medium">
+                                        Company <a className='text-gray-400'>...if you dare!</a>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        required
+                                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-blue-900 shadow-sm rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="font-medium">
+                                        Message <a className='text-gray-400'>...if not, why would you come here?</a>
+                                    </label>
+                                    <textarea name='message' required className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-blue-900 shadow-sm rounded-lg"></textarea>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="w-full px-4 py-2 text-white font-medium bg-blue-900 rounded-lg duration-150"
+                                >
+                                    Submit
+                                </button>
+                            </form>
+                            <span>{result}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </section>
   )
 }
